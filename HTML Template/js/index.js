7223f8f863dec3f653563d64d942b7040f755a6f
Ivan Panfilov <ivan.panf@yandex.ru>
2021-11-04 00:47:40 +0300

    добавил фронтенд

---
+++ HTML Template/js/index.js
@@ -0,0 +1,10 @@
+
+
+function showHide(Element) {
+  nextElement = Element.nextElementSibling;
+  if (nextElement.style.display != 'block') { 
+    nextElement.style.display = 'block';
+  } else { 
+    nextElement.style.display = 'none'; 
+  }
+}
---
+++ HTML Template/js/user.js
@@ -0,0 +1,7 @@
+
+var swiper = new Swiper(".swiper", {
+    initialSlide: 1,
+    pagination: {
+        el: ".swiper-pagination",
+    }
+});
